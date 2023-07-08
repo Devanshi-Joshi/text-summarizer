3f58db6dc0ba0451e6fd83fb8e8274cb007b9ed1
Mukund <mukund@Mukunds-MacBook-Air.local>
2023-07-09 03:23:16 +0530

    initial setup committs

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+with open ("README.md",'r',encoding='utf-8') as f :
+    long_desc=f.read()
+
+__version__='0.0.0'
+AUTHORS_NAME1 ='Devanshi '
+AUTHORS_NAME2= 'Mukundan'
+SRC_REPO='text-summarizer'
+
